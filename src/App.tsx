bb8f183cfac27ead51a637eaa472e9cd17753c60
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2023-03-02 16:50:25 -0300

    project base - login screen and attendance history implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Login } from './components/Login';
+import { AttendanceHistory } from './components/AttendanceHistory';
 
 function App() {
+  const [isLoggedIn, setLogged] = useState<boolean>(false)
+
+  useEffect(() => {
+    const employeeUsername = localStorage.getItem("employeeHistory");
+    if(employeeUsername) setLogged(true)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn ? <AttendanceHistory /> : <Login />}
     </div>
   );
 }
+
 
 export default App;
